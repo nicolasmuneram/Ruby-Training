f3aa0d20219a6bf90daaa7b020980054d5c495d6
Nicolas Munera <nmunera@eafit.edu.co>
2015-02-21 23:47:53 -0500

    Finished the course! Uploaded latest files

--- odot/app/models/todo_item.rb
+++ odot/app/models/todo_item.rb
@@ -2,9 +2,14 @@ class TodoItem < ActiveRecord::Base
 	belongs_to :todo_list
 
 	validates :content, presence: true,
-						length: { minimum: 2 }
+	length: { minimum: 2 }
 
-  def completed?
-  	!completed_at.blank?
-  end
+	scope :complete, -> { where.not(completed_at: nil) }
+  	scope :incomplete, -> { where(completed_at: nil) }
+	
+	def completed?
+		!completed_at.blank?
+	end
+
+	
 end
